373f71688b308577f2fd388f8475b149cdb10187
Code-Amare <amisgana809@gmail.com>
2026-09-21 16:18:14 +0300

    feat: add users urls template

---
+++ backend/users/urls.py
@@ -0,0 +1,27 @@
+from django.urls import path
+
+from .views import (
+    RegisterView,
+    LoginView,
+    LoginViaEmailView,
+    SendVerificationCodeView,
+    EmailVerifyView,
+    LogoutView,
+    RequestPasswordResetView,
+    ResetPasswordConfirmView,
+    ChangePasswordView,
+    EmailLoginRequestView,
+)
+
+urlpatterns = [
+    path("register/", RegisterView.as_view()),
+    path("login/", LoginView.as_view()),
+    path("logout/", LogoutView.as_view()),
+    path("login/email/request/", EmailLoginRequestView.as_view()),
+    path("login/email/<uuid:code>/", LoginViaEmailView.as_view()),
+    path("email/verify/request/", SendVerificationCodeView.as_view()),
+    path("email/verify/", EmailVerifyView.as_view()),
+    path("password/reset/request/", RequestPasswordResetView.as_view()),
+    path("password/reset/<uuid:code>/", ResetPasswordConfirmView.as_view()),
+    path("password/change/", ChangePasswordView.as_view()),
+]
