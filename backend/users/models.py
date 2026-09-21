a63ebe5bd3d268878039316bd36aefb86aa228c2
Code-Amare <amisgana809@gmail.com>
2026-09-21 18:12:41 +0300

    feat: add id and make email unique

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,21 @@
+import uuid
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False,
+    )
+
+    email = models.EmailField(
+        unique=True,
+    )
+
+    date_of_birth = models.DateField(
+        null=True,
+        blank=True,
+    )
