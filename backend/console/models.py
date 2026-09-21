533ba0ac2af280028d9da3e802e7992dcc432ac7
Code-Amare <amisgana809@gmail.com>
2026-09-21 17:50:58 +0300

    feat: add PlatformSettings model

--- backend/console/models.py
+++ backend/console/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class PlatformSettings(models.Model):
+
+    # Branding / identity
+    site_name = models.CharField(max_length=100, default="MegaLearn")
+    logo = CloudinaryField("logo", blank=True, null=True)
+    support_email = models.EmailField(blank=True, default="")
+    support_phone = models.CharField(max_length=32, blank=True, default="")
+
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        self.pk = 1
+        super().save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        pass
+
+    @classmethod
+    def get_solo(cls):
+        obj, _ = cls.objects.get_or_create(pk=1)
+        return obj
+
+    def __str__(self):
+        return "Platform Settings"
