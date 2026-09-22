3c14e3351b34cedaa2b6a567fa31e7561819c286
Code-Amare <amisgana809@gmail.com>
2026-09-22 18:23:18 +0300

    feat: add models to admin.py:

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import User, EmailLoginLink, EmailOTP, ResetPasswordLink
 
-# Register your models here.
+admin.site.register([User, EmailOTP, EmailLoginLink, ResetPasswordLink])
